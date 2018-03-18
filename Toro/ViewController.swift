4bc57038216faca15e90159e84d2faa2c5b46166
jblaine <jblaine>
2018-03-18 09:46:39 -0400

    Initial Commit

---
+++ Toro/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Toro
+//
+//  Created by John Blaine on 3/18/18.
+//  Copyright © 2018 John Blaine. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
